407dd0e6fc1e72dbfe77abb25fddb4e0da699b94
euronion <42553970+euronion@users.noreply.github.com>
2026-03-27 11:04:55 +0100

    Remove no longer required cleanup code

--- scripts/prepare_scenario_FBMC.py
+++ scripts/prepare_scenario_FBMC.py
@@ -20,24 +20,6 @@ from scripts._helpers import (
 
 logger = logging.getLogger(__name__)
 
-
-def fix_incorrect_operational_link_limits(n):
-    p_max_pu = n.c.links.dynamic.p_max_pu
-    p_min_pu = n.c.links.dynamic.p_min_pu
-
-    diff = p_max_pu - p_min_pu
-    diff = diff.where(diff < 0).dropna(axis=1, how="all")
-    if not diff.empty:
-        bad = diff.columns
-        too_bad = (abs(diff.loc[:, bad]) > 1e-6).any()
-        if too_bad.any():
-            raise ValueError(
-                f"p_min_pu considerably larger than p_max_pu for: {', '.join(too_bad.index[too_bad])}"
-            )
-
-        p_min_pu.loc[:, bad] = p_min_pu.loc[:, bad].clip(upper=p_max_pu.loc[:, bad])
-
-
 if __name__ == "__main__":
     if "snakemake" not in globals():
         from scripts._helpers import mock_snakemake
@@ -46,14 +28,16 @@ if __name__ == "__main__":
             Path(__file__).stem,
             planning_horizons=2030,
         )
-    # configure_logging(snakemake)
+    configure_logging(snakemake)
 
     n = pypsa.Network(snakemake.input["model"])
     n.name = (
         f"Flow-based market coupling (FBMC) - {snakemake.wildcards.planning_horizons}"
     )
 
-    fix_incorrect_operational_link_limits(n)
+    # No modifications required for the FBMC network
+    # The constraints are applied during the solving step
+    # so the network can just be written out again here without modifications
 
     # Doesn't hurt
     n.consistency_check(strict=None)
